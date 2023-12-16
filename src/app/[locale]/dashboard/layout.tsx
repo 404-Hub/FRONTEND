414f5e05b41c47fd3e6a2e36a9e380cac7d21330
Ilya Zhigarev <zhigarev@bk.ru>
2023-12-16 23:08:45 +0100

    feat: - move pages to [locale] folder - add next-intl package: configure i18nMiddleware, i18n - add LocaleContext and LocaleProvider to root component - add my-task page using internalization const t = useTranslations('my-tasks')

---
+++ src/app/[locale]/dashboard/layout.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+// import UserNavigation from '@/components/auth/user-navigation';
+
+// interface DashboardLayoutProps {
+//   children?: React.ReactNode;
+// }
+
+// export default function DashboardLayout({ children }: DashboardLayoutProps) {
+export default function DashboardLayout() {
+  return (
+    <div>
+      <header>
+        <div>
+          <Link href="/">Logo</Link>
+          {/* <UserNavigation /> */}
+        </div>
+      </header>
+
+      {/* <main>{children}</main> */}
+      <main></main>
+    </div>
+  );
+}
