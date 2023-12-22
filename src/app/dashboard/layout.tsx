c98421f42ece0d5e41693565d6dae4294d944902
Ilya Zhigarev <zhigarev@bk.ru>
2023-12-22 20:03:42 +0100

    fix: eslint errors

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,22 +1,23 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 // import UserNavigation from '@/components/auth/user-navigation';
+//
+// interface DashboardLayoutProps {
+//   children?: React.ReactNode;
+// }
 
-interface DashboardLayoutProps {
-  children?: React.ReactNode;
-}
-
-export default function DashboardLayout({ children }: DashboardLayoutProps) {
+// export default function DashboardLayout({ children }: DashboardLayoutProps) {
+export default function DashboardLayout() {
   return (
     <div>
-      <header>
-        <div>
-          <Link href="/">Logo</Link>
+       {/* <header> */}
+       {/* <div> */}
+       {/*   <Link href="/">Logo</Link> */}
 
-          {/*<UserNavigation />*/}
-        </div>
-      </header>
+       {/*   <UserNavigation /> */}
+       {/*  </div> */}
+       {/* </header> */}
 
-      <main>{children}</main>
+       {/* <main>{children}</main> */}
     </div>
   );
 }
