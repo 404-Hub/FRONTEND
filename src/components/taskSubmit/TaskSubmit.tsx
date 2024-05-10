dd661c45049689486737fa647e37e07c9ed4c770
Terenty-JS <143122309+terDev2023@users.noreply.github.com>
2024-05-10 21:29:25 +0300

    task submit

---
+++ src/components/taskSubmit/TaskSubmit.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import TaskSubmitForMobile from './taskSubmitForMobile/TaskSubmitForMobile';
+import TaskSubmitForDesctop from './taskSubmitForDesctop/TaskSubmitForDesctop';
+import useConditions from './hooks/useConditions';
+
+const TaskSubmit = () => {
+  const conditionsInf = useConditions();
+  const { conditions, handleChangeCondition, isValidCond } = conditionsInf;
+  return (
+    <>
+      <TaskSubmitForMobile
+        conditions={conditions}
+        handleChangeCondition={handleChangeCondition}
+        isValidCond={isValidCond}
+      />
+      <TaskSubmitForDesctop
+        conditions={conditions}
+        handleChangeCondition={handleChangeCondition}
+        isValidCond={isValidCond}
+      />
+    </>
+  );
+};
+
+export default TaskSubmit;
---
+++ types/taskSubmit.ts
@@ -0,0 +1,40 @@
+import { Dispatch, SetStateAction } from 'react';
+
+type TTaskSubmitingProps = {
+  validation: (isRight: boolean) => void;
+  submiting: () => void;
+  conditions: Record<string, boolean>;
+  handleChangeCondition: (condition: string, conditionValue?: boolean) => void;
+  isValidCond: boolean;
+};
+
+type TTaskSubmitingAfterClickProps = {
+  isValid: boolean;
+  submiting: () => void;
+};
+
+type TGithubAdd = {
+  validation: (isRight: boolean) => void;
+  githubCheck: (hasGit: boolean) => void;
+  submiting: () => void;
+};
+
+type TGithubInputProps = {
+  isInputValid: boolean;
+  setIsInputValid: Dispatch<SetStateAction<boolean>>;
+  isVisible?: boolean;
+};
+
+type TTaskSubmitProps = {
+  conditions: Record<string, boolean>;
+  handleChangeCondition: (condition: string, conditionValue?: boolean) => void;
+  isValidCond: boolean;
+};
+
+export type {
+  TTaskSubmitingProps,
+  TTaskSubmitingAfterClickProps,
+  TGithubAdd,
+  TGithubInputProps,
+  TTaskSubmitProps,
+};
